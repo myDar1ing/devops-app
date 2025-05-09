694cd3a20d0c5c960baef4ee254436afc899a911
Adilet <bbddbbd2003@gmail.com>
2025-05-09 22:36:17 +0500

    toCommit

--- app/main.py
+++ app/main.py
@@ -18,7 +18,7 @@ async def seed_if_needed():
 
 @app.get("/")
 async def home():
-    return {"msg": "Hello, world v3.  Ships to prod before your coffee cools ☕️"}
+    return {"msg": "Hello, world v3.  Ships to prod before your coffee cools ☕️ and stay cool"}
 
 @app.get("/users")
 async def users(db=Depends(get_db)):
